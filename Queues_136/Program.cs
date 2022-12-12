e83eeac62b60ea9dd7e498c7c65ea15b9c3fbdca
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-12-12 16:40:56 +0700

    buat program

--- Queues_136/Program.cs
+++ Queues_136/Program.cs
@@ -8,6 +8,22 @@ namespace Queues_136
 {
     class Program
     {
+        int FRONT, REAR, max = 5;
+        int[] queu_array = new int[5];
+
+        public Program()
+        {
+            /*initializing the value of the variable REAR and FRONT to -1 to indicate that
+             * the queue to initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            /*this statement check for the overflow  conditions. */
+            if((FRONT == 0 && REAR == max ))
+        }
+
         static void Main(string[] args)
         {
         }
