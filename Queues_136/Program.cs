9b0631707f53d05a61844fcdc62a31a0714790ab
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-12-12 16:50:41 +0700

    MEMBUAT PERBANDINGAN INSERT

--- Queues_136/Program.cs
+++ Queues_136/Program.cs
@@ -26,6 +26,23 @@ namespace Queues_136
                 Console.WriteLine("\nQueue overflow\n");
                 return;
             }
+            /*insert this following statement checks whether the queue is empty.if the queue is empty
+             * . then the value of the REAR and FRONT variabeles is set to 0. */
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* if REAR is at the last position of the array, then the value of
+                 * REAR is set to 0 that corresponde to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /* if REAR is not at the last position , then its value is incremented by one */
+                    REAR = REAR + 1;
+            }
         }
 
         static void Main(string[] args)
